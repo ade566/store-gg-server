ddeda2f6aa5784f5a83ab56373cb5e904e594e1c
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-02-13 21:35:40 +0700

    Mengelola halaman kategori (CRUD)→ Create

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,7 +1,28 @@
+const Category = require('./model')
+
 module.exports = {
   index: async (req, res) => {
     try {
       res.render('admin/category/index')
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  create: async (req, res) => {
+    try {
+      res.render('admin/category/create')
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  _create : async(req, res) => {
+    try {
+      const {name} = req.body
+
+      let category = await Category({name})
+      await category.save()
+
+      res.redirect('/category')
     } catch (error) {
       console.log(error);
     }
--- app/category/router.js
+++ app/category/router.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { index } = require('./controller')
+const { index, create, _create } = require('./controller')
 
 router.get('/', index);
+router.get('/create', create);
+router.post('/_create', _create);
 
 module.exports = router;

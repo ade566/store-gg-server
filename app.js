3433f3c449952a3a6828d4e39001a8f324a817b7
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-02-20 16:19:59 +0700

    Mengelola halaman signin admin

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var nominalRouter = require('./app/nominal/router');
 var voucherRouter = require('./app/voucher/router');
 var BankRouter = require('./app/bank/router');
 var paymentRouter = require('./app/payment/router');
+var userRouter = require('./app/users/router');
 var app = express();
 
 // view engine setup
@@ -34,7 +35,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/adminlte', express.static(path.join(__dirname, '/node_modules/admin-lte/')))
 
-app.use('/', dashboardRouter);
+app.use('/', userRouter);
+app.use('/dashboard', dashboardRouter);
 app.use('/category', categoryRouter);
 app.use('/nominal', nominalRouter);
 app.use('/voucher', voucherRouter);

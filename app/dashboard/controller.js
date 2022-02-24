a55b242a61eb1a72783bf6207f5b8394f365994d
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-02-24 21:32:04 +0700

    Mengelola halaman beranda

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,9 +1,22 @@
+const Transaction = require('../transaction/model')
+const Category = require('../category/model')
+const Voucher = require('../voucher/model')
+const Player = require('../player/model')
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render('index', {
+      const transaction = await Transaction.countDocuments()
+      const category = await Category.countDocuments()
+      const voucher = await Voucher.countDocuments()
+      const player = await Player.countDocuments()
+
+      res.render('admin/dashboard/index', {
         name: req.session.user.name,
-        title: 'Dashboard'
+        title: 'Dashboard',
+        count: {
+          transaction, category, voucher, player
+        }
       })
     } catch (error) {
       console.log(error);

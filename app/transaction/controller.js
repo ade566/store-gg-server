3d5a0a777985fe04d300ff79f07f382b08e91cc5
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-02-24 21:02:52 +0700

    Konfirmasi Transaksi dan Tolak Transaksi1~

--- app/transaction/controller.js
+++ app/transaction/controller.js
@@ -20,5 +20,20 @@ module.exports = {
       req.flash("alertStatus", "danger")
       res.redirect('/transaction')
     }
+  },
+  action: async (req, res) => {
+    try {
+      const {id} = req.params
+      const {status} = req.query
+
+      const data = await Transaction.findOneAndUpdate({_id: id}, {status})
+      req.flash('alertMessage', 'Berhasil ubah status')
+      req.flash('alertStatus', 'success')
+      res.redirect('/transaction')
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`)
+      req.flash("alertStatus", "danger")
+      res.redirect('/transaction')
+    }
   }
 }
--- app/transaction/router.js
+++ app/transaction/router.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const { index } = require('./controller')
+const { index, action } = require('./controller')
 const { isLoginAdmin } = require('../middleware/auth')
 
 router.use(isLoginAdmin)
 router.get('/', index);
+router.post('/status/:id', action);
 
 module.exports = router;

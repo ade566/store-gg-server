29da3a534ea982e8ee40237ee03de17d2df67e74
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-02-27 06:18:50 +0700

    Membuat api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -9,7 +9,24 @@ module.exports = {
 
       res.status(200).json({data: voucher})
     } catch (error) {
-      res.status(500).json({message: error.message || 'Terjadi kesalahan pada server'})
+      res.status(500).json({message: error.message || 'Internal server error'})
+    }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params
+      const voucher = await Voucher.findOne({_id: id})
+        .populate('category')
+        .populate('nominals')
+        .populate('user', '_id name phoneNumber')
+      
+      if(!voucher){
+        return res.status(404).json({message: "Voucher game not found.!"})
+      }
+
+      res.status(200).json({data: voucher})
+    } catch (error) {
+      res.status(500).json({message: error.message || 'Internal server error'})
     }
   }
 }

19074e29e86d830d57b4360a8e761b4cb7139646
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-02-27 16:40:40 +0700

    Membuat api signin

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,6 +2,8 @@ const Player = require('../player/model')
 const path = require('path')
 const fs = require('fs')
 const config = require('../../config')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
   signup : async (req, res,) => {
@@ -59,5 +61,45 @@ module.exports = {
       }
       next(err)
     }
+  },
+
+  signin : async (req, res, next) => {
+    const { email, password } = req.body
+    console.log(email);
+    console.log(password);
+    Player.findOne({email: email}).then((player) => {
+      if (player) {
+        const checkPassword = bcrypt.compareSync(password, player.password)
+        if (checkPassword) {
+          const token = jwt.sign({
+            player: {
+              id: player.id,
+              username: player.username,
+              email: player.email,
+              name: player.name,
+              phoneNumber: player.phoneNumber,
+              avatar: player.avatar,
+            }
+          }, config.jwtKey)
+          res.status(200).json({
+            data: token
+          })
+        } else {
+          res.status(403).json({
+            message: 'Password yang anda masukan salah'
+          })
+        }
+      } else {
+        res.status(403).json({
+          message: 'Email yang ada masukan belum terdaftar'
+        })
+      }
+    }).catch((err) => {
+      res.status(500).json({
+        message: err.message || 'Internal server error'
+      })
+
+      next()
+    })
   }
 }
